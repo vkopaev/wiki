f875f0b685047bcdd173c40a5450c57c62e111b7
Viacheslav Kopaev <viacheslav.kopaev@yandex.ru>
2026-04-20 22:42:28 +0300

    Update 2016_01_11_210908_add_external_auth_to_users.php

--- database/migrations/2016_01_11_210908_add_external_auth_to_users.php
+++ database/migrations/2016_01_11_210908_add_external_auth_to_users.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('external_auth_id')->index();
+            $table->string('external_auth_id')->nullable()->index();
         });
     }
 
